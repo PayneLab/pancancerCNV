21cf68ef59829e5fec42dd39141704b4c112a8ae
Caleb Lindgren <calebmlindgren@gmail.com>
2022-08-08 16:58:20 -0600

    Better slurm-auto-array setup

---
+++ 00_functions/chr08/08_01_run_ith_test.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# Get the index for this iteration
+IDX="$@"
+
+python 08_00_run_ith_test.py $IDX
--- 00_functions/chr08/08_02_slurm_auto_array.sh
+++ 00_functions/chr08/08_02_slurm_auto_array.sh
@@ -3,4 +3,4 @@
 find indices -type f | \
     slurm-auto-array --ntasks 4 --mem 4G --time 3:00:00 \
                      --mail-type BEGIN --mail-type END --mail-type FAIL --mail-user calebmlindgren@gmail.com \
-                     --command python 08_run_ith_test.py
+                     --command ./08_01_run_ith_test.sh
