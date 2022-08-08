f506b13340dff946cbe02dd660536b4f6fd3d96f
Caleb Lindgren <calebmlindgren@gmail.com>
2022-08-08 17:28:19 -0600

    Edit run parameters

--- 00_functions/chr08/08_02_slurm_auto_array.sh
+++ 00_functions/chr08/08_02_slurm_auto_array.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 find indices -type f | \
-    slurm-auto-array --ntasks 4 --mem 4G --time 3:00:00 \
+    slurm-auto-array --ntasks 4 --mem 8000M --time 3:00:00 \
                      -o output/slrm_%a.out \
                      --mail-type BEGIN --mail-type END --mail-type FAIL --mail-user calebmlindgren@gmail.com \
                      --command ./08_01_run_ith_test.sh
