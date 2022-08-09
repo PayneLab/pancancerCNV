1309d1772ed414c4fb8915d618148891f6e67029
Caleb Lindgren <calebmlindgren@gmail.com>
2022-08-09 11:34:54 -0600

    Prep split array syntax

--- 00_functions/chr08/09_01_raw_job_array_0_4999.sh
+++ 00_functions/chr08/09_01_raw_job_array_0_4999.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #SBATCH --time=6:00:00   # walltime
-#SBATCH --ntasks=1   # number of processor cores (i.e. tasks)
+#SBATCH --ntasks=2   # number of processor cores (i.e. tasks)
 #SBATCH --nodes=1   # number of nodes
 #SBATCH --mem-per-cpu=4000M   # memory per CPU core
 #SBATCH --mail-user=calebmlindgren@gmail.com   # email address
@@ -11,4 +11,5 @@
 #SBATCH --output=output_raw_split/slrm_%a.out 
 #SBATCH --array=0-4999
 
-python 09_00_run_ith_test.py $SLURM_ARRAY_TASK_ID
+python 09_00_run_ith_test.py $SLURM_ARRAY_TASK_ID &
+python 09_00_run_ith_test.py $((SLURM_ARRAY_TASK_ID + 5000)) &
