a22482fb469cbbd7819d84f9cdb372b33b8678a6
Caleb Lindgren <calebmlindgren@gmail.com>
2022-08-09 12:44:26 -0600

    Prep for raw array test

--- 00_functions/chr08/09_01_raw_job_array.sh
+++ 00_functions/chr08/09_01_raw_job_array.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
-#SBATCH --time=6:00:00   # walltime
+#SBATCH --time=2:00:00   # walltime
 #SBATCH --ntasks=2   # number of processor cores (i.e. tasks)
 #SBATCH --nodes=1   # number of nodes
-#SBATCH --mem-per-cpu=4000M   # memory per CPU core
+#SBATCH --mem-per-cpu=2000M   # memory per CPU core
 #SBATCH --mail-user=calebmlindgren@gmail.com   # email address
 #SBATCH --mail-type=BEGIN
 #SBATCH --mail-type=END
 #SBATCH --mail-type=FAIL
-#SBATCH --output=output_raw_split/slrm_%a.out 
+#SBATCH --output=output/slrm_%a.out 
 #SBATCH --array=0-4999
 
 python 09_00_run_ith_test.py $SLURM_ARRAY_TASK_ID &
 python 09_00_run_ith_test.py $((SLURM_ARRAY_TASK_ID + 5000)) &
+wait
