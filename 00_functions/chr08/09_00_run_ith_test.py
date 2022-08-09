7fe4e858a9c24671109420c561a3b2ced19baa86
Caleb Lindgren <calebmlindgren@gmail.com>
2022-08-09 09:57:43 -0600

    Add raw job array files

---
+++ 00_functions/chr08/09_00_run_ith_test.py
@@ -0,0 +1,39 @@
+import cnvutils
+import numpy as np
+import os
+import sys
+
+ENTROPY = 294341852078515767793057943394766554310
+
+# Get the index of this iteration
+i = int(sys.argv[1])
+
+# Generate the seed sequence for this iteration
+sseq = np.random.SeedSequence(entropy=ENTROPY, spawn_key=(i,))
+
+print(sseq.state)
+
+# Create an RNG from that seed sequence
+rng = np.random.default_rng(sseq)
+
+# Get the results for this permutation
+results = cnvutils.permute_props(
+    sources=["cptac", "gistic"],
+    levels=["gene"],
+    chromosomes_events={
+        8: {
+            "p": ["loss"],
+            "q": ["gain"],
+        },
+    },
+    rng=rng,
+)
+
+# Save the results to a temporary output directory
+save_dir = f"tmp{i // 1000:0>3}"
+os.makedirs(save_dir, exist_ok=True)
+
+results.to_csv(
+    os.path.join(save_dir, f"results_{i:0>6}.tsv"),
+    sep="\t",
+)
---
+++ 00_functions/chr08/09_01_run_ith_test.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+python 08_00_run_ith_test.py
