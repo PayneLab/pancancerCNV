dc9bb836a767e4a4cc126901948dd0a176545dee
Caleb Lindgren <calebmlindgren@gmail.com>
2022-08-09 11:02:22 -0600

    Split job array scripts

--- 00_functions/chr08/10_01_raw_job_array_7000_7999.sh
+++ 00_functions/chr08/10_01_raw_job_array_7000_7999.sh
@@ -1,17 +1,14 @@
 #!/bin/bash
 
-#SBATCH --time=4:00:00   # walltime
-#SBATCH --ntasks=11   # number of processor cores (i.e. tasks)
+#SBATCH --time=6:00:00   # walltime
+#SBATCH --ntasks=1   # number of processor cores (i.e. tasks)
 #SBATCH --nodes=1   # number of nodes
 #SBATCH --mem-per-cpu=4000M   # memory per CPU core
-#SBATCH --array=0-9998:11 # 9999 factors to 3*3*11*101
 #SBATCH --mail-user=calebmlindgren@gmail.com   # email address
 #SBATCH --mail-type=BEGIN
 #SBATCH --mail-type=END
 #SBATCH --mail-type=FAIL
-#SBATCH --output=output_raw/slrm_%a.out 
+#SBATCH --output=output_raw_split/slrm_%a.out 
+#SBATCH --array=7000-7999
 
-for i in {0..10}; do # run 11 processes in parallel
-     python 09_00_run_ith_test.py $((SLURM_ARRAY_TASK_ID + i)) &
-done
-wait
+python 10_00_run_ith_test.py $SLURM_ARRAY_TASK_ID
